7ddfe89d3a1b9cc918bd355bb238af9746cfacf1
Bulent824 <bulentozturk2019@outlook.com>
2020-05-13 16:58:01 +0200

    update CreateChannel.java

--- src/day_1/CreateChannel.java
+++ src/day_1/CreateChannel.java
@@ -5,5 +5,12 @@ public class CreateChannel {
     public static void main(String[] args) {
 
         System.out.println("User is able to create channels");
+
+        int x =5; //changed from Intellij
+
+        if (x==5){
+
+            System.out.println("true");
+        }
     }
 }
